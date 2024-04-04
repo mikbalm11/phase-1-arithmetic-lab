90849d92d2216f0b96c9dc354bb34f4662e636de
Mustafa Mehmedoglu <mikbalm@gmail.com>
2024-04-03 22:50:15 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let multiply;
+const num1 = 31;
+const num2 = 2;
+multiply = num1 * num2;
 
-// Write your code here
+let random;
+random = Math.floor(Math.random() * 100) + 1;
+
+let mod;
+const num3 = 24;
+const num4 = 5;
+mod = num3 % num4;
+
+let max;
+max = Math.max(4, 8, 12, 16, 20);
